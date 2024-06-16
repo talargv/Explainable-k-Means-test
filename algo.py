e38a65c4271e718abbcfcc1e349f66fa8baf0e15
Tal <talargov1@mail.tau.ac.il>
2024-06-16 23:48:28 +0300

    with experiments

--- algo.py
+++ algo.py
@@ -18,6 +18,9 @@ class ExplainableTree(ABC):
         self.tree = Tree()
 
     @abstractmethod
+    def metric(self, n, l_correct, r_correct, l_centers, r_centers):
+        pass
+
     def single_partition(self, data, clustering, centers, k, present_labels):
         """Decide a single axis-aligned threshold cut for data given clusters and centers.
 
@@ -44,9 +47,72 @@ class ExplainableTree(ABC):
 
         threshold: int - threshold of cut
         """
-        pass
+        assert k > 1
 
-    def extend(self, data, clustering, centers, present_labels, cut):
+        if np.intersect1d(clustering, present_labels).size == 0:
+            print("Warning: Given centers have no points represented in the data\nNo cuts will be generated further")
+            return np.inf, 0, np.NINF
+
+        n = data.shape[0] # num of samples
+        assert n > 0
+        d = data.shape[1] # dimension of data
+        best_ratio = np.inf # Best ratio with respect to metric
+        coordinate, threshold = 0, np.NINF 
+        u, clusters_count = np.unique(clustering, return_counts=True)
+        num_of_labels = u.size # since the tree is build recursively, data separated from it's cluster will not have a cluster present.        
+        mapping = dict(zip(u.tolist(), range(num_of_labels))) # since the tree is built recursively, a numbering 0,1,... is not guaranteed.
+                                                              # clusters_count[i] = num of samples in cluster mapping[i]
+        
+        for i in range(d):
+            data_ordering = data[:,i].argsort()
+            centers_ordering = centers[:,i].argsort()
+            curr_col = data[data_ordering, i]
+            clustering_ordered = clustering[data_ordering]
+            centers_ordered = centers[centers_ordering]
+            indices_ordered = present_labels[centers_ordering]
+
+            centers_threshold = -1 # final center (ordered by i'th coordinate) that is to the left of the cut.
+            samples_location = np.zeros(num_of_labels) # samples_location[i] = number of samples that belongs to cluster mapping[i] in left side of cut.
+            
+            for j in range(n):
+                samples_location[mapping[clustering_ordered[j]]] += 1 # x_j was moved to the left side of the cut.
+                while centers_threshold < k-1 and centers_ordered[centers_threshold+1, i] <= curr_col[j]:
+                    centers_threshold += 1
+                
+                if centers_threshold >= 0:
+                    left_side_clusters = [mapping[c] for c in indices_ordered[:centers_threshold+1]] # This sucks
+                    l_correct = np.sum(samples_location[left_side_clusters]) 
+                else:
+                    continue
+
+                if centers_threshold < k-1:
+                    right_side_clusters = [mapping[c] for c in indices_ordered[centers_threshold+1:]] # This sucks
+                    r_correct = np.sum((clusters_count-samples_location)[right_side_clusters]) 
+                else:
+                    continue
+
+                l_centers, r_centers = len(left_side_clusters), len(right_side_clusters)
+
+                ratio = self.metric(n, l_correct, r_correct, l_centers, r_centers)
+                
+
+                if ratio < best_ratio:
+                    coordinate, threshold = i, curr_col[j]
+                    best_ratio = ratio
+        return best_ratio, coordinate, threshold
+
+    def extend(self, data, clustering, centers, present_labels, cut, indices):
+        """Recursively builds the cuts tree.
+        
+        Input: 
+        data: np.ndarray - An array of size (number of samples) x (dimension of data) of the samples that reached the current node.
+        clustering: np.ndarray - An array of size (number of samples) of the labeling for data
+        centers: np.ndarray - An array of size (number of clusters) x (dimension of data) of the centers that reached the current node.
+        present_labels: np.ndarray - An array of size (number of clusters) of the labels of the centers that reached current node.
+        cut: Cut - the current node.
+        indices: np.ndarray - An array of size (number of clusters) that stores labels for the clusters.
+                              This exists to store the clustering of the data according to the tree.
+        """
         k = centers.shape[0]
         if k == 1:
             center = np.sum(data, axis=0) / data.shape[0]
@@ -54,186 +120,71 @@ class ExplainableTree(ABC):
 
             self.cost += cost
             self.centers[self.k, :] = center
+            self.clustering[indices] = self.k
             self.k += 1 
             return
         
-        _, coordinate, threshold = self.single_partition(data, clustering, centers, k, present_labels)
+        best_ratio, coordinate, threshold = self.single_partition(data, clustering, centers, k, present_labels)
+        if np.isinf(best_ratio):
+            print("Warning: Failed to get a cut.")
+            center = np.sum(data, axis=0) / data.shape[0]
+            cost = np.sum(np.linalg.norm(data - center, axis=1) ** 2)
+
+            self.cost += cost
+            self.centers[self.k, :] = center
+            self.clustering[indices] = self.k
+            self.k += 1 
+            return
+
         cut.coordinate, cut.threshold = coordinate, threshold
         cut.left, cut.right = Cut(), Cut()
 
         l_data_partition = data[:,coordinate] <= threshold
         l_data = data[l_data_partition]
         l_clustering = clustering[l_data_partition]
+        l_indices = indices[l_data_partition]
         l_centers_partition = centers[:,coordinate] <= threshold
         l_centers = centers[l_centers_partition]
         l_labels = present_labels[l_centers_partition]
 
         assert l_centers.shape[0] > 0
 
-        self.extend(l_data, l_clustering, l_centers, l_labels, cut.left)
+        self.extend(l_data, l_clustering, l_centers, l_labels, cut.left, l_indices)
 
         r_data_ordering = data[:,coordinate] > threshold
         r_data = data[r_data_ordering]
         r_clustering = clustering[r_data_ordering]
+        r_indices = indices[r_data_ordering]
         r_centers_partition = centers[:,coordinate] > threshold
         r_centers = centers[r_centers_partition]
         r_labels = present_labels[r_centers_partition]
 
         assert r_centers.shape[0] > 0
 
-        self.extend(r_data, r_clustering, r_centers, r_labels, cut.right)
+        self.extend(r_data, r_clustering, r_centers, r_labels, cut.right, r_indices)
 
     def train(self, data, clustering, centers):
+        """Build the tree of cuts."""
         k = centers.shape[0]
         self.centers = np.zeros_like(centers)
+        self.clustering = np.zeros_like(clustering)
         self.k, self.cost = 0, 0
-        self.extend(data, clustering, centers, np.arange(k), self.tree.root)
+        self.extend(data, clustering, centers, np.arange(k), self.tree.root, np.arange(clustering.size))
 
 class Spectral(ExplainableTree):
-    def single_partition(self, data, clustering, centers, k, present_labels):
-        assert k > 1
+    def metric(self, n, l_correct, r_correct, l_centers, r_centers):
+        if l_correct == 0 or r_correct == 0:
+            return np.inf
+        return (n - l_correct - r_correct) / min(l_correct, r_correct)
 
-        n = data.shape[0] # num of samples
-        d = data.shape[1] # dimension of data
-        best_ratio = np.inf # M/min(C1, C2)
-        coordinate, threshold = 0, np.NINF 
-        u, clusters_count = np.unique(clustering, return_counts=True)
-        num_of_labels = u.size # since the tree is build recursively, data separated from it's cluster will not have a cluster present.        
-        mapping = dict(zip(u.tolist(), range(num_of_labels))) # since the tree is built recursively, a numbering 0,1,... is not guaranteed.
-                                                              # clusters_count[i] = num of samples in cluster mapping[i]
-        
-        for i in range(d):
-            data_ordering = data[:,i].argsort()
-            centers_ordering = centers[:,i].argsort()
-            curr_col = data[data_ordering, i]
-            clustering_ordered = clustering[data_ordering]
-            centers_ordered = centers[centers_ordering]
-            indices_ordered = present_labels[centers_ordering]
-
-            centers_threshold = -1 # final center (ordered by i'th coordinate) that is to the left of the cut.
-            samples_location = np.zeros(num_of_labels) # samples_location[i] = number of samples that belongs to cluster mapping[i] in left side of cut.
-            
-            for j in range(n):
-                samples_location[mapping[clustering_ordered[j]]] += 1 # x_j was moved to the left side of the cut.
-                while centers_threshold < k-1 and centers_ordered[centers_threshold+1, i] <= curr_col[j]:
-                    centers_threshold += 1
-                
-                if centers_threshold >= 0:
-                    left_side_clusters = [mapping[c] for c in indices_ordered[:centers_threshold+1]] # This sucks
-                    C1 = np.sum(samples_location[left_side_clusters]) 
-                else:
-                    continue
-
-                if centers_threshold < k-1:
-                    right_side_clusters = [mapping[c] for c in indices_ordered[centers_threshold+1:]] # This sucks
-                    C2 = np.sum((clusters_count-samples_location)[right_side_clusters]) 
-                else:
-                    continue
-
-                ratio = (n-C1-C2) / min(C1, C2)
-                
-
-                if ratio < best_ratio:
-                    coordinate, threshold = i, curr_col[j]
-                    best_ratio = ratio
-        #print(best_ratio, coordinate, threshold)
-        return best_ratio, coordinate, threshold
     
 class IMM(ExplainableTree):
-    def single_partition(self, data, clustering, centers, k, present_labels):
-        assert k > 1
+    def metric(self, n, l_correct, r_correct, l_centers, r_centers):
+        return n - l_correct - r_correct
 
-        n = data.shape[0] # num of samples
-        d = data.shape[1] # dimension of data
-        best_ratio = np.inf # mistakes
-        coordinate, threshold = 0, np.NINF 
-        u, clusters_count = np.unique(clustering, return_counts=True)
-        num_of_labels = u.size # since the tree is build recursively, data separated from it's cluster will not have a cluster present.        
-        mapping = dict(zip(u.tolist(), range(num_of_labels))) # since the tree is built recursively, a numbering 0,1,... is not guaranteed.
-                                                              # clusters_count[i] = num of samples in cluster mapping[i]
-        
-        for i in range(d):
-            data_ordering = data[:,i].argsort()
-            centers_ordering = centers[:,i].argsort()
-            curr_col = data[data_ordering, i]
-            clustering_ordered = clustering[data_ordering]
-            centers_ordered = centers[centers_ordering]
-            indices_ordered = present_labels[centers_ordering]
 
-            centers_threshold = -1 # final center (ordered by i'th coordinate) that is to the left of the cut.
-            samples_location = np.zeros(num_of_labels) # samples_location[i] = number of samples that belongs to cluster mapping[i] in left side of cut.
-            
-            for j in range(n):
-                samples_location[mapping[clustering_ordered[j]]] += 1 # x_j was moved to the left side of the cut.
-                while centers_threshold < k-1 and centers_ordered[centers_threshold+1, i] <= curr_col[j]:
-                    centers_threshold += 1
-                
-                if centers_threshold >= 0:
-                    left_side_clusters = [mapping[c] for c in indices_ordered[:centers_threshold+1]] # This sucks
-                    C1 = np.sum(samples_location[left_side_clusters]) 
-                else:
-                    continue
-
-                if centers_threshold < k-1:
-                    right_side_clusters = [mapping[c] for c in indices_ordered[centers_threshold+1:]] # This sucks
-                    C2 = np.sum((clusters_count-samples_location)[right_side_clusters]) 
-                else:
-                    continue
-                
-                ratio = n-C1-C2
-                
-                if ratio < best_ratio:
-                    coordinate, threshold = i, curr_col[j]
-                    best_ratio = ratio
-        #print(best_ratio, coordinate, threshold)
-        return best_ratio, coordinate, threshold
-    
 class EMN(ExplainableTree):
-    def single_partition(self, data, clustering, centers, k, present_labels):
-        assert k > 1
-
-        n = data.shape[0] # num of samples
-        d = data.shape[1] # dimension of data
-        best_ratio = np.inf # mistakes
-        coordinate, threshold = 0, np.NINF 
-        u, clusters_count = np.unique(clustering, return_counts=True)
-        num_of_labels = u.size # since the tree is build recursively, data separated from it's cluster will not have a cluster present.        
-        mapping = dict(zip(u.tolist(), range(num_of_labels))) # since the tree is built recursively, a numbering 0,1,... is not guaranteed.
-                                                              # clusters_count[i] = num of samples in cluster mapping[i]
-        
-        for i in range(d):
-            data_ordering = data[:,i].argsort()
-            centers_ordering = centers[:,i].argsort()
-            curr_col = data[data_ordering, i]
-            clustering_ordered = clustering[data_ordering]
-            centers_ordered = centers[centers_ordering]
-            indices_ordered = present_labels[centers_ordering]
-
-            centers_threshold = -1 # final center (ordered by i'th coordinate) that is to the left of the cut.
-            samples_location = np.zeros(num_of_labels) # samples_location[i] = number of samples that belongs to cluster mapping[i] in left side of cut.
-            
-            for j in range(n):
-                samples_location[mapping[clustering_ordered[j]]] += 1 # x_j was moved to the left side of the cut.
-                while centers_threshold < k-1 and centers_ordered[centers_threshold+1, i] <= curr_col[j]:
-                    centers_threshold += 1
-                
-                if centers_threshold >= 0:
-                    left_side_clusters = [mapping[c] for c in indices_ordered[:centers_threshold+1]] # This sucks
-                    C1 = np.sum(samples_location[left_side_clusters]) 
-                else:
-                    continue
-
-                if centers_threshold < k-1:
-                    right_side_clusters = [mapping[c] for c in indices_ordered[centers_threshold+1:]] # This sucks
-                    C2 = np.sum((clusters_count-samples_location)[right_side_clusters]) 
-                else:
-                    continue
-                
-                ratio = (n-C1-C2) / min(len(left_side_clusters), len(right_side_clusters))
-                
-                if ratio < best_ratio:
-                    coordinate, threshold = i, curr_col[j]
-                    best_ratio = ratio
-        #print(best_ratio, coordinate, threshold)
-        return best_ratio, coordinate, threshold
-    
+    def metric(self, n, l_correct, r_correct, l_centers, r_centers):
+        if l_centers == 0 or r_centers == 0:
+            return np.inf
+        return (n - l_correct - r_correct) / min(l_centers, r_centers)
--- play.py
+++ play.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 
 from sklearn.datasets import make_blobs
 from sklearn.cluster import KMeans
+import sklearn.metrics as skm
 
 def plot_cut(cut: Cut, x_box, y_box, ax):
     if cut.left == None and cut.right == None:
@@ -28,11 +29,36 @@ def get_cost(data):
     median_of_data = np.sum(data, axis=0) / data.shape[0]
     return np.sum(np.linalg.norm(data - median_of_data, axis=1) ** 2)
 
+def get_k_cost(data, clustering, k):
+    k_cost = 0
+    for i in range(k):
+        k_cost += get_cost(data[clustering == i, :])
+    return k_cost    
+
+def get_weighted_center_dist(data, clustering, centers, k):
+    weighted_center_distance = 0
+    for i in range(k):
+        cluster_i_size = data[clustering == i, :].shape[0]
+        for j in range(k):
+            cluster_j_size = data[clustering == j, :].shape[0]
+            center_dist = np.linalg.norm(centers[i,:] - centers[j,:]) ** 2
+
+            weighted_center_distance += cluster_i_size * cluster_j_size * center_dist
+    return weighted_center_distance / data.shape[0]
+
+def get_clusterability(data, clustering_true, centers, k):
+    k_cost = get_k_cost(data, clustering_true, k)
+    one_cost = get_cost(data)
+    weighted_center_distance = get_weighted_center_dist(data, clustering_true, centers, k)
+
+    return (k_cost / (one_cost + weighted_center_distance)) ** 0.5
+
 def train_and_plot(data, clustering, centers):
+    k = centers.shape[0]
     box = (np.min(data), np.max(data)) # data might be out of box.
 
     fig, axes = plt.subplots(nrows=1, ncols=3, figsize=(30,15))
-    plt.figure(0)
+    #plt.figure(0)
     colors = plt.cm.hsv(np.linspace(0,1,k))
 
     for i in range(3):
@@ -65,6 +91,31 @@ def train_and_plot(data, clustering, centers):
 
     plt.show()
 
+def eval_model(data, clustering_true, centers, tree_type, show=False):
+    k = centers.shape[0]
+    tree = tree_type()
+    tree.train(data, clustering_true, centers)
+    clustering = tree.clustering
+
+    if show:
+        tree_name = tree_type.__name__
+        print(f"Metrics for {tree_name}:")
+        print(f"    Silhouette score: {skm.silhouette_score(data, clustering)}")
+        print(f"    Price of Explainability: {get_k_cost(data, clustering, k) / get_k_cost(data, clustering_true, k)}")
+    
+        assert abs(get_k_cost(data, clustering, k) - tree.cost) < 0.003
+    return tree.cost
+
+def vanilla_kmeans_metrics(data, clustering_true, centers, k, show=False):
+    silouette = skm.silhouette_score(data, clustering_true)
+    clusterability = get_clusterability(data, clustering_true, centers, k)
+    if show:
+        print("Metric for Vanilla k-Means:")
+        print(f"    Silhouette score: {silouette}")
+        print(f"    Clusterability: {clusterability}")
+
+    return silouette, clusterability
+
 def show_cost(data, clustering, centers):
     tree = IMM()
     tree.train(data, clustering, centers)
@@ -78,39 +129,98 @@ def show_cost(data, clustering, centers):
     tree.train(data, clustering, centers)
     print(f"EMN cost: {tree.cost}")
 
+def experiment_k(n_runs):
+    models = [IMM, Spectral, EMN]
+    NUM_OF_MODELS = len(models)
+    seed = None
+    k_list = [4, 10, 25, 50]
+    box = (-5, 5) # The range of the coordinates of the centers
+    std = 1 # Variance
+    n_samples = 4000
+    dimension = 10
 
-seed = None
-k = 30
-box = (-20, 20)
-std = 5
-n_samples = 4000
-dimension = 50
+    s_avgs = np.zeros(len(k_list))
+    c_avgs = np.zeros(len(k_list))
+    prices_avgs = np.zeros((NUM_OF_MODELS, len(k_list)))
+    
+    for (l, k) in enumerate(k_list):
+        s_scores = np.zeros(n_runs)
+        c_values = np.zeros(n_runs)
+        prices = np.zeros((NUM_OF_MODELS, n_runs))
 
-data, clustering, centers = make_blobs(n_samples=n_samples, centers=k, cluster_std=std,
-                                        random_state=seed, return_centers=True, n_features=dimension, center_box=box)
-model = KMeans(n_clusters = k, init = centers).fit(data)
+        for i in range(n_runs):
+            data, _, centers = make_blobs(n_samples=n_samples, centers=k, cluster_std=std,
+                                                    random_state=seed, return_centers=True, n_features=dimension, center_box=box)
+            model = KMeans(n_clusters = k, init = centers).fit(data)
 
-clustering = model.labels_
-centers = model.cluster_centers_
+            clustering_true = model.labels_
+            centers = model.cluster_centers_
 
-one_cost = get_cost(data)
-k_cost = 0
-for i in range(k):
-    k_cost += get_cost(data[clustering == i, :])
-weighted_center_distance = 0
-for i in range(k):
-    cluster_i_size = data[clustering == i, :].shape[0]
-    for j in range(k):
-        cluster_j_size = data[clustering == j, :].shape[0]
-        center_dist = np.linalg.norm(centers[i,:] - centers[j,:]) ** 2
+            s_scores[i], c_values[i] = vanilla_kmeans_metrics(data, clustering_true, centers, k)
 
-        weighted_center_distance += cluster_i_size * cluster_j_size * center_dist
-weighted_center_distance /= data.shape[0]
+            k_cost = get_k_cost(data, clustering_true, k)
 
-print(f"k-Cost: {k_cost}")
-print(f"1-Cost: {one_cost}")
-print(f"Thingy: {weighted_center_distance}")
-print(f"Clusterability: {k_cost / (one_cost + weighted_center_distance)}")
+            for (j, model) in enumerate(models):
+                prices[j,i] = eval_model(data, clustering_true, centers, model) / k_cost
 
+        s_avgs[l] = np.sum(s_scores) / n_runs
+        c_avgs[l] = np.sum(c_values) / n_runs
+        prices_avgs[:,l] = np.sum(prices, axis=1) / n_runs
 
-show_cost(data, clustering, centers)
+    _, axes = plt.subplots(nrows=1, ncols=3, figsize=(30,15))
+    axes[0].plot(k_list, prices_avgs[0,:], color="red") #IMM
+    axes[0].plot(k_list, prices_avgs[1,:], color="blue") #Spectral
+    axes[0].plot(k_list, prices_avgs[2,:], color="green") #EMN
+    axes[1].plot(k_list, s_avgs)
+    axes[2].plot(k_list, c_avgs)
+
+    plt.show()
+
+def experiment_std(n_runs):
+    models = [IMM, Spectral, EMN]
+    NUM_OF_MODELS = len(models)
+    seed = None
+    k = 20
+    box = (-5, 5) # The range of the coordinates of the centers
+    std_list = [10,5,2,1,0.5] # Variance
+    n_samples = 4000
+    dimension = 10
+
+    s_avgs = np.zeros(len(std_list))
+    c_avgs = np.zeros(len(std_list))
+    prices_avgs = np.zeros((NUM_OF_MODELS, len(std_list)))
+    
+    for (l, std) in enumerate(std_list):
+        s_scores = np.zeros(n_runs)
+        c_values = np.zeros(n_runs)
+        prices = np.zeros((NUM_OF_MODELS, n_runs))
+
+        for i in range(n_runs):
+            data, _, centers = make_blobs(n_samples=n_samples, centers=k, cluster_std=std,
+                                                    random_state=seed, return_centers=True, n_features=dimension, center_box=box)
+            model = KMeans(n_clusters = k, init = centers).fit(data)
+
+            clustering_true = model.labels_
+            centers = model.cluster_centers_
+
+            s_scores[i], c_values[i] = vanilla_kmeans_metrics(data, clustering_true, centers, k)
+
+            k_cost = get_k_cost(data, clustering_true, k)
+
+            for (j, model) in enumerate(models):
+                prices[j,i] = eval_model(data, clustering_true, centers, model) / k_cost
+
+        s_avgs[l] = np.sum(s_scores) / n_runs
+        c_avgs[l] = np.sum(c_values) / n_runs
+        prices_avgs[:,l] = np.sum(prices, axis=1) / n_runs
+
+    _, axes = plt.subplots(nrows=1, ncols=3, figsize=(30,15))
+    axes[0].plot(std_list, prices_avgs[0,:], color="red") #IMM
+    axes[0].plot(std_list, prices_avgs[1,:], color="blue") #Spectral
+    axes[0].plot(std_list, prices_avgs[2,:], color="green") #EMN
+    axes[1].plot(std_list, s_avgs)
+    axes[2].plot(std_list, c_avgs)
+
+    plt.show()
+
+experiment_std(15)
